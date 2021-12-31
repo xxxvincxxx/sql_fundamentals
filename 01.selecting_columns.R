6d9c618f2b7217be7ba5cc727ecfb9e394ec0f87
xxxvincxxx <vincenzo.grass@gmail.com>
2021-12-31 11:00:01 +0100

    test

--- 01.selecting_columns.R
+++ 01.selecting_columns.R
@@ -1,1 +1,8 @@
-test
+library(credentials)
+
+usethis::use_git_config(user.name = "xxxvincxxx", user.email = "vincenzo.grass@gmail.com")
+usethis::browse_github_token()
+credentials::set_github_pat("R:GITHUB_PAT")
+
+
+usethis::edit_r_environ()
