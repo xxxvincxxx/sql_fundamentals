46f3568fb35690e4c91fe0682c5cebfde9327fb4
xxxvincxxx <vincenzo.grass@gmail.com>
2021-12-31 11:01:16 +0100

    rename

--- 00.Connect_git_to_RStudio.R
+++ 00.Connect_git_to_RStudio.R
@@ -1,8 +1,7 @@
-library(credentials)
+## Connect RStudio to Github via .Renviron
 
 usethis::use_git_config(user.name = "xxxvincxxx", user.email = "vincenzo.grass@gmail.com")
 usethis::browse_github_token()
 credentials::set_github_pat("R:GITHUB_PAT")
-
 
 usethis::edit_r_environ()
