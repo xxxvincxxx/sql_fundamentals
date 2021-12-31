ba8a7ccc6c01b27631b45630119920ea81d51492
xxxvincxxx <vincenzo.grass@gmail.com>
2021-12-31 11:17:04 +0100

    --

--- 00.Connect_git_to_RStudio.R
+++ 00.Connect_git_to_RStudio.R
@@ -1,4 +1,5 @@
 ## Connect RStudio to Github via .Renviron
+# https://gist.github.com/Z3tt/3dab3535007acf108391649766409421
 
 usethis::use_git_config(user.name = "xxxvincxxx", user.email = "vincenzo.grass@gmail.com")
 usethis::browse_github_token()
@@ -7,5 +8,5 @@ credentials::set_github_pat("R:GITHUB_PAT")
 usethis::edit_r_environ()
 
 # Add commits to a previous date 
-
-git commit --date="10 day ago" -m "Your commit message" 
+# test
+# git commit --date="7 day ago" -m "Last try adding previous date git commit" 
