ab40ec62b8ad984b1eb35a4bb658393f3dcebe86
xxxvincxxx <vincenzo.grass@gmail.com>
2022-02-05 19:08:34 +0100

    test

--- 00.Connect_git_to_RStudio.R
+++ 00.Connect_git_to_RStudio.R
@@ -3,7 +3,7 @@
 
 usethis::use_git_config(user.name = "xxxvincxxx", user.email = "vincenzo.grass@gmail.com")
 usethis::browse_github_token()
-credentials::set_github_pat("R:GITHUB_PAT")
+credentials::set_github_pat("R:GITHUB_PAT1")
 
 usethis::edit_r_environ()
 
